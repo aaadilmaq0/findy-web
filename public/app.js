efef49ac8d8f5aa5ec6856892b6bc5afb6b9b0d4
aaadilmaq0 <aaadilmaq0@gmail.com>
2017-12-19 15:54:00 +0200

    added destination click events

---
+++ public/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyALIzNOAKDQldh_Lu8j0FxMMF30teazWqk",
+authDomain: "findy-b69e4.firebaseapp.com",
+databaseURL: "https://findy-b69e4.firebaseio.com",
+projectId: "findy-b69e4",
+storageBucket: "findy-b69e4.appspot.com",
+messagingSenderId: "227043781907"
+};
+firebase.initializeApp(config);
