e5da4d3fda8b6053a5e2d8ceb816ed194d6cb609
aaadilmaq0 <aaadilmaq0@gmail.com>
2018-01-04 17:14:38 +0200

    made lots of changes

--- public/destinations/app.js
+++ public/destinations/app.js
@@ -8,6 +8,11 @@ storageBucket: "findy-b69e4.appspot.com",
 messagingSenderId: "227043781907"
 };
 firebase.initializeApp(config);
-var parameters = location.search.substring(1).split("&");
-var temp= parameters[0].split("=");
-var p=temp[1];
+
+
+  var parameters = location.search.substring(1).split("&");
+  var temp1= parameters[0].split("=");
+  var category=temp1[1];
+  var temp2= parameters[1].split("=");
+  var place=temp2[1];
+  console.log(place+" "+category);
